0ce0904ce27dff0242b8a41262e8b095df53ff12
Олег Кравченко <oleg.kravchenko88@gmail.com>
2016-12-05 12:41:34 +0500

    Remove filter initializer

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 /* jshint node: true */
 'use strict';
 
-var filterInitializers = require('fastboot-filter-initializers');
 
 module.exports = {
-  name: 'ember-ajax-cached',
-
-  preconcatTree: function(tree) {
-    return filterInitializers(tree, this.app.name);
-  }
+  name: 'ember-ajax-cached'
 };
